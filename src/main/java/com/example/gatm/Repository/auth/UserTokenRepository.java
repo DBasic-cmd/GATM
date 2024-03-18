b7d9690446fa3aafa9f58359c717316bb40f7ce5
Naomiel <naomieloor@gmail.com>
2024-03-18 22:44:36 +0100

    What I have done so far on the Task management app

---
+++ src/main/java/com/example/gatm/Repository/auth/UserTokenRepository.java
@@ -0,0 +1,9 @@
+package com.example.gatm.Repository.auth;
+
+import com.example.gatm.model.auth.UserToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserTokenRepository extends JpaRepository<UserToken, UUID> {
+}
